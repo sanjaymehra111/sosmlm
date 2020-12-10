df7d30786f9bafb4d4c0c960ce3ce132cb5957e4
sanjay mehra <sanjaymehra111@gmail.com>
2020-12-10 15:00:01 +0530

    war created

---
+++ src/test/java/com/sosmlm/controller/SosmlmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sosmlm.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SosmlmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
